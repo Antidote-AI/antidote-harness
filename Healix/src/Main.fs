933839b6ec5f820f0af8edc1db885d439f43aa78
Alex Campo <alex.campo@antidote-ai.com>
2023-06-13 14:47:37 -0400

    appt list viewer

--- Healix/src/Main.fs
+++ Healix/src/Main.fs
@@ -4,11 +4,12 @@ open Browser.Dom
 open Antidote.Core.V2.Types
 open Feliz
 open Healix.Components.Sample
+open Healix.Components.AppointmentList
 
 // emitJsStatement () "import React from \"react\""
 // importSideEffects "./index.scss"
 
 ReactDOM.render(
-    Sample ()
+    ListItem ()
     , document.getElementById("root")
 )
