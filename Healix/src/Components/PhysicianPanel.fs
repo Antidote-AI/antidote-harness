7e4848302ae725e058b1c2925f5b6b3d1ead8202
Alex Campo <alex.campo@antidote-ai.com>
2023-10-03 23:00:17 -0400

    media q's for physician panel

--- Healix/src/Components/PhysicianPanel.fs
+++ Healix/src/Components/PhysicianPanel.fs
@@ -48,18 +48,18 @@ let PhysicianPanel () =
     let (heartSelected, setHeartSelected) = React.useState(false)
 
     Html.section [
-        prop.classes ["card"; "m-4"]
+        prop.classes ["card"; "m-3"]
         prop.children [
             Html.div [
-                //prop.style [style.custom("boxShadow", "rgba(0, 0, 0, 0.16) 0px 1px 4px")]
-                prop.classes ["is-flex"; "is-flex-direction-row"; "is-justify-content-space-between";]
+                prop.classes ["is-flex"; "is-flex-direction-row"; "is-justify-content-space-between"; classes.mobileFlexWrap] // Add mobileFlexWrap class here
                 prop.children [
                     Html.div [
+                        prop.className [classes.mobileCenterImage]
                         prop.children [
                             Html.img [
                                 prop.src ".././Assets/alan-katz.jpg"
                                 prop.alt "doctor's profile logo"
-                                prop.classes [ classes.DoctorImage; "image"; "p-1"; "ml-1"]
+                                prop.classes [ classes.DoctorImage; "image"; "p-1"; "ml-1"; classes.imageResizing]
                                 prop.style [style.borderRadius 10; style.width 65; style.height 75; style.marginTop 5; style.marginBottom 5; style.marginRight 5]
                             ]
                         ]
@@ -68,10 +68,11 @@ let PhysicianPanel () =
                         prop.style [style.flexGrow 1; style.display.flex; style.justifyContent.center; style.flexDirection.column]
                         prop.children [
                             Html.div [
-                                prop.style [ style.borderBottom(1, borderStyle.solid, "#F0F0F0"); style.width (length.perc 97); style.alignItems.center ]
-                                prop.classes ["is-flex"; "is-flex-direction-row"; "is-justify-content-space-between"]
+                                prop.style [ style.borderBottom(1, borderStyle.solid, "#F0F0F0"); style.width (length.perc 97); style.alignItems.center; style.justifyContent.spaceBetween ]
+                                prop.classes ["is-flex"; "is-flex-direction-row"; classes.mobileCenterItems]
                                 prop.children [
                                     Html.h2 [
+                                        prop.style [ ]
                                         prop.classes ["has-text-weight-bold"]
                                         prop.text "Dr. Alan Katz"
                                         prop.style [style.fontWeight.bold; style.fontSize 20]
@@ -100,83 +101,56 @@ let PhysicianPanel () =
                                 ]
                             ]
                             Html.div [
+                                prop.className [classes.mobileFlexWrap; classes.centerDiv]
                                 prop.style [style.display.flex; style.justifyContent.spaceBetween; style.width (length.perc 97)]
                                 prop.children [
                                     Html.div [
                                         prop.children [
                                             Html.p [
-                                                //prop.classes ["ml-1"]
+                                                prop.className [classes.centerDiv]
                                                 prop.style [style.color.dimGray; style.fontSize 16; style.display.flex; style.flexDirection.row]
                                                 prop.children [
-                                                    Html.p [
-                                                        prop.style [style.marginLeft 0;]
-                                                        prop.text "Psychiatrist"
-                                                    ]
-                                                    Html.p [
-                                                        prop.style [style.marginLeft 5;]
-                                                        prop.text "|"
-                                                    ]
-                                                    Html.p [
-                                                        prop.style [style.marginLeft 5;]
-                                                        prop.text "HARC"
-                                                    ]
+                                                    Html.p [prop.text "Psychiatrist"]
+                                                    Html.p [prop.text "|"]
+                                                    Html.p [prop.text "HARC"]
                                                 ]
                                             ]
                                             Html.div [
                                                 prop.style [style.display.flex; style.flexDirection.row; style.alignItems.center]
                                                 prop.children [
-                                                    Html.img [
-                                                        prop.src ".././Assets/star-yellow.svg"
-                                                        prop.style [
-                                                            style.width 15
-                                                            style.height 15
-                                                        ]
-                                                    ]
-                                                    Html.p [
-                                                        prop.style [style.color.black; style.fontSize 16; style.marginLeft 3; style.fontWeight.bold]
-                                                        prop.text "4.9"
-                                                    ]
-                                                    Html.p [
-                                                        prop.style [style.color.gray; style.fontSize 16; style.marginLeft 8]
-                                                        prop.text "(4,279 reviews)"
-                                                    ]
+                                                    Html.img [prop.src ".././Assets/star-yellow.svg"; prop.style [style.width 15; style.height 15]]
+                                                    Html.p [prop.style [style.color.black; style.fontSize 16; style.marginLeft 3; style.fontWeight.bold]; prop.text "4.9"]
+                                                    Html.p [prop.style [style.color.gray; style.fontSize 16; style.marginLeft 8]; prop.text "(4,279 reviews)"]
                                                 ]
                                             ]
                                         ]
                                     ]
                                     Html.div [
+                                        prop.className [classes.mobileFlexWrap; classes.centerDiv; classes.columnStyle]
                                         prop.children [
                                             Html.div [
+                                                prop.className [classes.centerDiv]
                                                 prop.style [style.display.flex; style.flexDirection.row; style.alignItems.center; style.justifyContent.flexEnd; style.color.black; style.fontSize 16]
                                                 prop.children [
-                                                    Html.i [
-                                                        prop.style [style.marginRight 3; style.paddingTop 4]
+                                                    Icon [icon.icon mdi.location; icon.color "black"; icon.width 18]
+                                                    Html.div [
+                                                        prop.style [style.marginLeft 3]
                                                         prop.children [
-                                                            Icon [
-                                                                icon.icon mdi.location
-                                                                icon.color "black"
-                                                                icon.width 18
-                                                            ]
+                                                            Html.text "7.2 Miles"
                                                         ]
                                                     ]
-                                                    Html.text "7.2 Miles"
                                                 ]
                                             ]
                                             Html.div [
-                                                prop.className [classes.mobileFlexWrap; classes.mobileBorderTop]
-                                                prop.style [style.display.flex; style.flexDirection.row; style.alignItems.center; style.justifyContent.flexEnd; style.color.black; style.color.black; style.fontSize 16]
+                                                prop.style [style.display.flex; style.flexDirection.row; style.alignItems.center; style.justifyContent.flexEnd; style.color.black; style.fontSize 16]
                                                 prop.children [
-                                                    Html.i [
-                                                        prop.style [style.marginRight 3; style.paddingTop 4]
+                                                    Icon [icon.icon mdi.hospitalBuilding; icon.color "black"; icon.width 18]
+                                                    Html.div [
+                                                        prop.style [style.marginLeft 3]
                                                         prop.children [
-                                                            Icon [
-                                                                icon.icon mdi.hospitalBuilding
-                                                                icon.color "black"
-                                                                icon.width 18
-                                                            ]
+                                                            Html.text "2020 N Bayshore Drive 1806"
                                                         ]
                                                     ]
-                                                    Html.text "2020 N Bayshore Drive, Miami, FL 33137"
                                                 ]
                                             ]
                                         ]
